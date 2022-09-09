d5894b143e53d283dbf316585008ccd64248cd7e
Kate Zhukouskaya <ars.astashevich@gmail.com>
2022-09-09 11:10:49 +0300

    Render dta from sql in react

--- Panda/ClientApp/src/App.js
+++ Panda/ClientApp/src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { Counter } from './components/Counter';
 import { CreateNewHouse } from './components/CreateNewHouse';
 //import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
-
 import './custom.css'
+import { ListHouses } from './components/ListHouses';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -17,7 +16,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
+        <Route path='/cabins' component={ListHouses} />
         <Route path='/create-new-house' component={CreateNewHouse} />
         <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
       </Layout>
